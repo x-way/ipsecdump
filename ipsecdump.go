ddc09a1a782022c1680c4ba351f127915884e30c
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2019-09-08 16:25:41 +0200

    Add tests

--- ipsecdump.go
+++ ipsecdump.go
@@ -4,37 +4,38 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"github.com/x-way/pktdump"
 	"log"
+	"net"
 	"os"
 	"os/exec"
 	"time"
+
+	"github.com/x-way/pktdump"
 
 	nflog "github.com/florianl/go-nflog"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 )
 
-var nflogGroup = flag.Int("g", 5050, "NFLOG group to use")
-var dumpDuration = flag.Duration("t", 10*time.Second, "how long to run the NFLOG dumping")
-var mode = flag.String("m", "tunnel", "IPSec mode (tunnel or transport)")
-var tunnelSource = flag.String("s", "", "IPSec tunnel source IP")
-var tunnelDestination = flag.String("d", "", "IPSec tunnel destination IP")
-var iface = flag.String("i", "any", "incoming interface to listen on (default: any)")
-
 func main() {
+
+	var (
+		dumpDuration      = flag.Duration("t", 10*time.Second, "how long to run the NFLOG dumping")
+		iface             = flag.String("i", "any", "incoming interface to listen on (default: any)")
+		mode              = flag.String("m", "tunnel", "IPSec mode (tunnel or transport)")
+		tunnelSource      = flag.String("s", "", "IPSec tunnel source IP")
+		tunnelDestination = flag.String("d", "", "IPSec tunnel destination IP")
+		nflogGroup        = flag.Int("g", 5050, "NFLOG group to use")
+	)
+
 	flag.Parse()
 
-	prefix := fmt.Sprintf("ipsecdump:%d", os.Getpid())
+	if err := validateFlags(*mode, *tunnelSource, *tunnelDestination); err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
 
-	if *mode != "tunnel" && *mode != "transport" {
-		fmt.Println("Error: -m parameter only supports 'tunnel' or 'transport'")
-		return
-	}
-	if *mode == "transport" && ((*tunnelSource != "") || (*tunnelDestination != "")) {
-		fmt.Println("Error: transport mode does not support tunnel source/destination IPs")
-		return
-	}
+	prefix := fmt.Sprintf("ipsecdump:%d", os.Getpid())
 
 	config := nflog.Config{
 		Group:       uint16(*nflogGroup),
@@ -70,20 +71,8 @@ func main() {
 		log.Fatal(fmt.Sprintf("Could not register nflog callback: %v\n", err))
 	}
 
-	params := []string{"PREROUTING", "-t", "raw", "-m", "policy", "--dir", "in", "--pol", "ipsec", "--mode", *mode, "--proto", "esp"}
-	if *iface != "any" {
-		params = append(params, "-i", *iface)
-	}
-	if *tunnelSource != "" {
-		params = append(params, "--tunnel-src", *tunnelSource)
-	}
-	if *tunnelDestination != "" {
-		params = append(params, "--tunnel-dst", *tunnelDestination)
-	}
-	params = append(params, "-j", "NFLOG", "--nflog-group", fmt.Sprintf("%d", *nflogGroup), "--nflog-prefix", prefix)
-
-	addCmd := exec.Command("iptables", append([]string{"-I"}, params...)...)
-	delCmd := exec.Command("iptables", append([]string{"-D"}, params...)...)
+	addCmd := exec.Command("iptables", buildIptablesParams(false, *mode, *iface, *tunnelSource, *tunnelDestination, *nflogGroup, prefix)...)
+	delCmd := exec.Command("iptables", buildIptablesParams(true, *mode, *iface, *tunnelSource, *tunnelDestination, *nflogGroup, prefix)...)
 
 	defer func() {
 		if err := delCmd.Run(); err != nil {
@@ -95,4 +84,49 @@ func main() {
 	}
 
 	<-ctx.Done()
+}
+
+func validateFlags(mode, tunnelSource, tunnelDestination string) error {
+	if mode != "tunnel" && mode != "transport" {
+		return fmt.Errorf("mode must be 'tunnel' or 'transport'")
+	}
+	if mode == "transport" && ((tunnelSource != "") || (tunnelDestination != "")) {
+		return fmt.Errorf("transport mode does not support tunnel source/destination IPs")
+	}
+	if tunnelSource != "" && net.ParseIP(tunnelSource) == nil {
+		return fmt.Errorf("tunnel source IP must be a valid IP address")
+	}
+	if tunnelDestination != "" && net.ParseIP(tunnelDestination) == nil {
+		return fmt.Errorf("tunnel destination IP must be a valid IP address")
+	}
+	return nil
+}
+
+func buildIptablesParams(del bool, mode, iface, tunnelSource, tunnelDestination string, nflogGroup int, prefix string) []string {
+	var params []string
+
+	if del {
+		params = append(params, "-D")
+	} else {
+		params = append(params, "-I")
+	}
+
+	params = append(params, []string{"PREROUTING", "-t", "raw"}...)
+	if iface != "any" {
+		params = append(params, "-i", iface)
+	}
+
+	params = append(params, []string{"-m", "policy", "--dir", "in", "--pol", "ipsec", "--mode", mode, "--proto", "esp"}...)
+	if mode == "tunnel" {
+		if tunnelSource != "" {
+			params = append(params, "--tunnel-src", tunnelSource)
+		}
+		if tunnelDestination != "" {
+			params = append(params, "--tunnel-dst", tunnelDestination)
+		}
+	}
+
+	params = append(params, "-j", "NFLOG", "--nflog-group", fmt.Sprintf("%d", nflogGroup), "--nflog-prefix", prefix)
+
+	return params
 }
---
+++ ipsecdump_test.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestValidatFlags(t *testing.T) {
+	tests := map[string]struct {
+		mode              string
+		tunnelSource      string
+		tunnelDestination string
+		expected          string
+	}{
+		"tunnel mode":                                       {"tunnel", "", "", ""},
+		"transport mode":                                    {"transport", "", "", ""},
+		"invalid mode":                                      {"invalid", "", "", "mode must be 'tunnel' or 'transport'"},
+		"transport mode with tunnel source IP":              {"transport", "1.2.3.4", "", "transport mode does not support tunnel source/destination IPs"},
+		"transport mode with tunnel destination IP":         {"transport", "", "1.2.3.4", "transport mode does not support tunnel source/destination IPs"},
+		"tunnel mode with tunnel source IP":                 {"tunnel", "1.2.3.4", "", ""},
+		"tunnel mode with tunnel destination IP":            {"tunnel", "", "1.2.3.4", ""},
+		"tunnel mode with tunnel source and destination IP": {"tunnel", "1.2.3.4", "1.2.3.4", ""},
+		"tunnel mode with invalid tunnel source IP":         {"tunnel", "1.2.3.333", "", "tunnel source IP must be a valid IP address"},
+		"tunnel mode with invalid tunnel destination IP":    {"tunnel", "", "1.2.3.4.5", "tunnel destination IP must be a valid IP address"},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := validateFlags(tt.mode, tt.tunnelSource, tt.tunnelDestination)
+			gotstr := fmt.Sprintf("%s", got)
+			if got == nil {
+				gotstr = ""
+			}
+			if tt.expected != gotstr {
+				t.Errorf("expected: %s, got: %s", tt.expected, gotstr)
+			}
+		})
+	}
+}
+
+func TestBuildIptablesParams(t *testing.T) {
+	tests := map[string]struct {
+		del               bool
+		mode              string
+		iface             string
+		tunnelSource      string
+		tunnelDestination string
+		nflogGroup        int
+		prefix            string
+		expected          []string
+	}{
+		"insert simple transport filter": {
+			false, "transport", "any", "", "", 5050, "myprefix",
+			[]string{"-I", "PREROUTING", "-t", "raw", "-m", "policy", "--dir", "in", "--pol", "ipsec", "--mode", "transport", "--proto", "esp", "-j", "NFLOG", "--nflog-group", "5050", "--nflog-prefix", "myprefix"},
+		},
+		"remove simple transport filter": {
+			true, "transport", "any", "", "", 5050, "myprefix",
+			[]string{"-D", "PREROUTING", "-t", "raw", "-m", "policy", "--dir", "in", "--pol", "ipsec", "--mode", "transport", "--proto", "esp", "-j", "NFLOG", "--nflog-group", "5050", "--nflog-prefix", "myprefix"},
+		},
+		"insert eth0 transport filter": {
+			false, "transport", "eth0", "", "", 5050, "myprefix",
+			[]string{"-I", "PREROUTING", "-t", "raw", "-i", "eth0", "-m", "policy", "--dir", "in", "--pol", "ipsec", "--mode", "transport", "--proto", "esp", "-j", "NFLOG", "--nflog-group", "5050", "--nflog-prefix", "myprefix"},
+		},
+		"remove eth0 transport filter": {
+			true, "transport", "eth0", "", "", 5050, "myprefix",
+			[]string{"-D", "PREROUTING", "-t", "raw", "-i", "eth0", "-m", "policy", "--dir", "in", "--pol", "ipsec", "--mode", "transport", "--proto", "esp", "-j", "NFLOG", "--nflog-group", "5050", "--nflog-prefix", "myprefix"},
+		},
+		"insert simple tunnel filter": {
+			false, "tunnel", "any", "", "", 5050, "myprefix2",
+			[]string{"-I", "PREROUTING", "-t", "raw", "-m", "policy", "--dir", "in", "--pol", "ipsec", "--mode", "tunnel", "--proto", "esp", "-j", "NFLOG", "--nflog-group", "5050", "--nflog-prefix", "myprefix2"},
+		},
+		"remove simple tunnel filter": {
+			true, "tunnel", "any", "", "", 5050, "myprefix2",
+			[]string{"-D", "PREROUTING", "-t", "raw", "-m", "policy", "--dir", "in", "--pol", "ipsec", "--mode", "tunnel", "--proto", "esp", "-j", "NFLOG", "--nflog-group", "5050", "--nflog-prefix", "myprefix2"},
+		},
+		"insert eth0 tunnel filter": {
+			false, "tunnel", "eth0", "", "", 4040, "myprefix",
+			[]string{"-I", "PREROUTING", "-t", "raw", "-i", "eth0", "-m", "policy", "--dir", "in", "--pol", "ipsec", "--mode", "tunnel", "--proto", "esp", "-j", "NFLOG", "--nflog-group", "4040", "--nflog-prefix", "myprefix"},
+		},
+		"remove eth0 tunnel filter": {
+			true, "tunnel", "eth0", "", "", 4040, "myprefix",
+			[]string{"-D", "PREROUTING", "-t", "raw", "-i", "eth0", "-m", "policy", "--dir", "in", "--pol", "ipsec", "--mode", "tunnel", "--proto", "esp", "-j", "NFLOG", "--nflog-group", "4040", "--nflog-prefix", "myprefix"},
+		},
+		"insert eth0 tunnel filter with tunnelsource": {
+			false, "tunnel", "eth0", "1.2.3.4", "", 5050, "myprefix",
+			[]string{"-I", "PREROUTING", "-t", "raw", "-i", "eth0", "-m", "policy", "--dir", "in", "--pol", "ipsec", "--mode", "tunnel", "--proto", "esp", "--tunnel-src", "1.2.3.4", "-j", "NFLOG", "--nflog-group", "5050", "--nflog-prefix", "myprefix"},
+		},
+		"remove eth0 tunnel filter with tunnelsource": {
+			true, "tunnel", "eth0", "1.2.3.4", "", 5050, "myprefix",
+			[]string{"-D", "PREROUTING", "-t", "raw", "-i", "eth0", "-m", "policy", "--dir", "in", "--pol", "ipsec", "--mode", "tunnel", "--proto", "esp", "--tunnel-src", "1.2.3.4", "-j", "NFLOG", "--nflog-group", "5050", "--nflog-prefix", "myprefix"},
+		},
+		"insert eth0 tunnel filter with tunnelsource and destination": {
+			false, "tunnel", "eth0", "1.2.3.4", "4.5.6.7", 5050, "myprefix",
+			[]string{"-I", "PREROUTING", "-t", "raw", "-i", "eth0", "-m", "policy", "--dir", "in", "--pol", "ipsec", "--mode", "tunnel", "--proto", "esp", "--tunnel-src", "1.2.3.4", "--tunnel-dst", "4.5.6.7", "-j", "NFLOG", "--nflog-group", "5050", "--nflog-prefix", "myprefix"},
+		},
+		"remove eth0 tunnel filter with tunnelsource and destination": {
+			true, "tunnel", "eth0", "1.2.3.4", "4.5.6.7", 5050, "myprefix",
+			[]string{"-D", "PREROUTING", "-t", "raw", "-i", "eth0", "-m", "policy", "--dir", "in", "--pol", "ipsec", "--mode", "tunnel", "--proto", "esp", "--tunnel-src", "1.2.3.4", "--tunnel-dst", "4.5.6.7", "-j", "NFLOG", "--nflog-group", "5050", "--nflog-prefix", "myprefix"},
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := buildIptablesParams(tt.del, tt.mode, tt.iface, tt.tunnelSource, tt.tunnelDestination, tt.nflogGroup, tt.prefix)
+			diff := cmp.Diff(tt.expected, got)
+			if diff != "" {
+				t.Errorf(diff)
+			}
+		})
+	}
+}
